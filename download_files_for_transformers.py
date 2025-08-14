26ab38cadc0ca25fd0ca3fec60409e7d5eacb505
Player1444 <94734830+Player124413@users.noreply.github.com>
2025-08-14 14:25:51 +0300

    Update download_files_for_transformers.py

--- download_files_for_transformers.py
+++ download_files_for_transformers.py
@@ -10,8 +10,8 @@ os.makedirs(assets_folder, exist_ok=True)
 repo_url = "https://huggingface.co/Politrees/RVC_resources/resolve/main"
 file_urls = {
     "rmvpe/rmvpe.pt": f"{repo_url}/predictors/rmvpe.pt",
-    "hubert/config.json": f"{repo_url}/embedders/transformers/{embedder_name}/config.json",
-    "hubert/pytorch_model.bin": f"{repo_url}/embedders/transformers/{embedder_name}/pytorch_model.bin",
+    "hubert/config.json": f"https://huggingface.co/IAHispano/Applio/resolve/main/Resources/embedders/{embedder_name}/config.json",
+    "hubert/pytorch_model.bin": f"https://huggingface.co/IAHispano/Applio/resolve/main/Resources/embedders/{embedder_name}/pytorch_model.bin",
 }
 
 for file, url in file_urls.items():
