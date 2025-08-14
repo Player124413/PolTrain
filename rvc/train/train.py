a25a5f21caf5e5b5392337dd6fca7cbde049b4c2
Player1444 <94734830+Player124413@users.noreply.github.com>
2025-08-14 15:50:58 +0300

    Update train.py

--- rvc/train/train.py
+++ rvc/train/train.py
@@ -188,7 +188,7 @@ def run(hps, rank, n_gpus, device, device_id):
         if vocoder == "RingFormer":
         # MPD + MSD + MRD
         from rvc.lib.algorithm.discriminators.multi.mpd_msd_mrd_combined import MPD_MSD_MRD_Combined
-        net_d = MPD_MSD_MRD_Combined(config.model.use_spectral_norm, use_checkpointing=use_checkpointing, **dict(config.mrd))
+        net_d = MPD_MSD_MRD_Combined(config.model.use_spectral_norm, checkpointing=False, **dict(config.mrd))
         else:
             net_d = MultiPeriodDiscriminator(hps.model.use_spectral_norm, checkpointing=False)
 
