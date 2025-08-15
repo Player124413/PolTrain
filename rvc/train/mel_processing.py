3ac57d8788b1193b036164c4555714da6ef8d72d
Player1444 <94734830+Player124413@users.noreply.github.com>
2025-08-15 12:42:16 +0300

    Update mel_processing.py

--- rvc/train/mel_processing.py
+++ rvc/train/mel_processing.py
@@ -20,7 +20,7 @@ def spectral_de_normalize_torch(magnitudes):
 
 
 mel_basis = {}
-hann_window = {}
+hann_window = torch.hann_window(hps.model.gen_istft_n_fft).to(device)
 
 
 def spectrogram_torch(y, n_fft, hop_size, win_size, center=False):
