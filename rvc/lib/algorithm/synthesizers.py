aaaaada0dc506301add693784c928f78c7cb7f1d
Player1444 <94734830+Player124413@users.noreply.github.com>
2025-08-15 13:27:30 +0300

    Update synthesizers.py

--- rvc/lib/algorithm/synthesizers.py
+++ rvc/lib/algorithm/synthesizers.py
@@ -184,14 +184,14 @@ class Synthesizer(torch.nn.Module):
             if self.randomized:
                 z_slice, ids_slice = rand_slice_segments(z, y_lengths, self.segment_size)
                 pitchf = slice_segments(pitchf, ids_slice, self.segment_size, 2)
-                o = self.dec(z_slice, pitchf, g=g)
+                o, spec, phase = self.dec(z_slice, pitchf, g=g)
                 return o, ids_slice, x_mask, y_mask, (z, z_p, m_p, logs_p, m_q, logs_q)
             # future use for finetuning using the entire dataset each pass
             else:
-                o = self.dec(z, pitchf, g=g)
-                return o, None, x_mask, y_mask, (z, z_p, m_p, logs_p, m_q, logs_q)
+                 o, spec, phase = self.dec(z_slice, pitchf, g=g)
+                return o, None, x_mask, y_mask, (z, z_p, m_p, logs_p, m_q, logs_q), (spec, phase)
         else:
-            return None, None, x_mask, None, (None, None, m_p, logs_p, None, None)
+            return None, None, x_mask, None, (None, None, m_p, logs_p, None, None), (spec, phase)
 
     @torch.jit.export
     def infer(
