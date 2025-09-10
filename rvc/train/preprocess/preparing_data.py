8f43a60e87ce9c8c913eebb199a72b8d7d00add3
Player1444 <94734830+Player124413@users.noreply.github.com>
2025-09-10 18:54:40 +0300

    Update preparing_data.py
    Добавил Harvest

--- rvc/train/preprocess/preparing_data.py
+++ rvc/train/preprocess/preparing_data.py
@@ -67,6 +67,15 @@ class DataPreprocessor:
         audio = load_audio(path, self.sample_rate)
         if f0_method == "rmvpe":
             return self.model_rmvpe.infer_from_audio(audio, 0.03)
+        if f0_method == "harvest":
+            f0, t = pyworld.harvest(
+                x.astype(np.double),
+                fs=self.fs,
+                f0_ceil=self.f0_max,
+                f0_floor=self.f0_min,
+                frame_period=1000 * self.hop / self.fs,
+            )
+            f0 = pyworld.stonemask(x.astype(np.double), f0, t, self.fs)
         elif f0_method == "rmvpe+":
             return self.model_rmvpe.infer_from_audio_modified(audio, 0.02)
 
