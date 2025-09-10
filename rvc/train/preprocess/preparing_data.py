46a34c6175cc167ef7f1d5fe1c5cba40a8fb07ee
Player1444 <94734830+Player124413@users.noreply.github.com>
2025-09-10 20:45:28 +0300

    Update preparing_data.py

--- rvc/train/preprocess/preparing_data.py
+++ rvc/train/preprocess/preparing_data.py
@@ -65,6 +65,16 @@ class DataPreprocessor:
     def compute_f0(self, path, f0_method):
         """Вычисление F0"""
         audio = load_audio(path, self.sample_rate)
+        if f0_method == "harvest":
+            f0, t = pyworld.harvest(
+                audio.astype(np.double),
+                fs=self.sample_rate,
+                f0_ceil=self.f0_max,
+                f0_floor=self.f0_min,
+                frame_period=1000 * self.hop_size / self.sample_rate,
+            )
+            f0 = pyworld.stonemask(audio.astype(np.double), f0, t, self.sample_rate)
+
         if f0_method == "rmvpe":
             return self.model_rmvpe.infer_from_audio(audio, 0.03)
         elif f0_method == "rmvpe+":
